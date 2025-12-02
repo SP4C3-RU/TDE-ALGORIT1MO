4344f7c1d6ffd05690a592ad9bf3af985ba1f220
SP4C3-RU <andreycosta059@gmail.com>
2025-12-02 10:06:41 -0300

    Update tde.py

--- tde.py
+++ tde.py
@@ -7,3 +7,13 @@ def mostrar_menu():
   print("5 - Remover tarefa ")
   print("6 - Sair")
   return input("Escolha uma opção:")
+
+def listar_tarefas(tarefas):
+  if len(tarefas) == 1:
+    print("\nNenhuma tarefa cadastrada.")
+  else:
+    print("\n=== LISTA DE TAREFAS ===")
+    for i, tarefa in enumerate(tarefas):
+      status = "CONCLUÍUDA" if tarefa["status"] else "PENDENTE"
+      print(f"{i}-{tarefa["descricao"]} ({status})")
+    
